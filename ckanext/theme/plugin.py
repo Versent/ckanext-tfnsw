af4b0bf86e72140a36a60810a63f8fce54f582b1
Sanoob thekke Valappil <89111433+sanoobthekke@users.noreply.github.com>
2024-07-19 11:50:34 +1000

    updating resource format names

--- ckanext/theme/plugin.py
+++ ckanext/theme/plugin.py
@@ -6,7 +6,6 @@ from .cli import get_commands
 class ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer, inherit=True)
     plugins.implements(plugins.IClick)
-    plugins.implements(plugins.ITemplateHelpers)
 
     def get_commands(self):
         return get_commands()
@@ -15,7 +14,3 @@ class ThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "theme")
-
-    def get_helpers(self):
-        from ckanext.theme.helpers import register_helpers
-        return register_helpers()
