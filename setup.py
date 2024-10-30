18c8386ac80b05c6036d0a59c63b84e971968722
Robert Sturrock <78390241+rns-vs@users.noreply.github.com>
2024-10-30 14:19:22 +1100

    Turn off the tfnsw_s3view plugin as a test

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
     tfnsw_customauth=ckanext.customauth.plugin:CustomAuthPlugin
     tfnsw_discourse=ckanext.discourse.plugin:DiscoursePlugin
     tfnsw_openapiview=ckanext.openapiview.plugin:OpenApiViewPlugin
-    tfnsw_s3view=ckanext.s3view.plugin:S3ViewPlugin
     tfnsw_s3browse=ckanext.s3browse.plugin:S3BrowsePlugin
     tfnsw_subscriptions=ckanext.subscriptions.plugin:SubscriptionsPlugin
     tfnsw_metrics=ckanext.metrics.plugin:MetricsPlugin
